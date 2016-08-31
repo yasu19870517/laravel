baf11b09a879b611dd19d9d3e581cfa3fcd6432d
yasu19870517 <yasu19870517@gmail.com>
2016-08-31 02:52:49 +0000

    Install crud-generator

--- config/app.php
+++ config/app.php
@@ -177,7 +177,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Appzcoder\CrudGenerator\CrudGeneratorServiceProvider::class,
     ],
 
     /*
@@ -224,7 +224,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Form' => Collective\Html\FormFacade::class,
+        'HTML' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
